59ccf667595691f86bc832372e447c21e55121b1
tayyaba-ali <ta6402792@gmail.com>
2025-09-02 12:40:05 +0500

    class 2

---
+++ Module 2/javascript/class2/app.js
@@ -0,0 +1,47 @@
+// Question # 01 
+// alert("hello user"); //parenthesis
+
+// Question # 03
+
+// alert("Welcome to Js Land \n Happy Coding")
+
+// console.log("hello js")
+// document.write("we are writing on white space")
+
+
+/*jsdfkfds
+sdfhlfds
+dsfjlk*/
+// kjhdsakjhdsa
+
+
+// data types
+
+// string  "hjdsfhkfds" "338bdafkfds" "#$4325hsdkjf" 
+// number 35456456
+// boolean  true 
+// undefined
+// null
+// Symbol
+// Biginit
+// Object
+
+
+var studentName = "Tayyaba"
+var subject = 31566564564564
+// assignment operator
+
+// console.log(studentName)
+// alert(studentName)
+// document.write(studentName)
+
+var studentName = 543545
+var studentName = true
+// reassignment 
+
+
+lastName = "ali"
+lastName= "irshad"
+
+// console.log(studentName)
+console.log(lastName);
