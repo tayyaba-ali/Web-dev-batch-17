70b720e7c6b2cd7479725133560706a82b2c6ef5
tayyaba-ali <ta6402792@gmail.com>
2025-09-04 12:25:16 +0500

    js class 3 done

---
+++ Module 2/javascript/class3/app.js
@@ -0,0 +1,90 @@
+
+// syntax
+// var studentName = "Tayyaba" //declaration+ initialization
+
+// var username; //declare
+
+
+// var message;
+// message = "Hello World"
+
+// var studentName = "Nabiha"
+// var studentCourse = "Web dev"
+// var studentAge = "16 years"
+
+// alert(studentName)
+// alert(studentCourse)
+// alert(studentAge)
+
+
+var $studentname = 123
+
+// console.log(userPassword);
+
+// illegal
+// you can't use the js keywords as var names
+// var names can't be start with numbers
+// space is not allowed in naming variables
+// special characters are not allowed except _ and $ 
+
+// console.log($studentname);
+
+// myNameIsRuhama
+
+// username
+// firstname last name"
+// email address validation
+// get user by id
+// calculate total amount
+// is valid input
+// convert to json string
+// http response code
+// xml http request
+// database connection pool
+
+
+// let myPhoneNumber = 544645645
+// myPhoneNumber = 4456
+
+// /dfsjklfds
+// dsf;jfds;
+// dsfljds;falsedsf;ljdsf
+// ds;lfjd;sf
+// fjfds
+// jfds
+
+
+// let myPhoneNumber = 254545
+// console.log(myPhoneNumber);
+
+
+// const pi = 3.142
+// const pi = 23145
+// pi = 4456
+
+// var phone ;
+
+// let phoneNumber;
+// const pi;
+
+
+
+var firstName = "Tayyaba";
+var lastName = "Ali"
+   
+
+
+
+
+
+
+document.write("My name is "+firstName+" "+ lastName)
+document.write("<br />")
+document.write(56+"a23356")
+
+// "key    board"
+
+var age = 15
+
+console.log("I am "+age+" years old  ")
+alert("I am "+age+" years old  ")
