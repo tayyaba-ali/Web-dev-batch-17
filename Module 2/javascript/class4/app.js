9d9eabe79f0c6bec08cfd05df1a86e5807de7ae1
tayyaba-ali <ta6402792@gmail.com>
2025-09-09 11:52:25 +0500

    js class 4

---
+++ Module 2/javascript/class4/app.js
@@ -0,0 +1,33 @@
+
+
+
+
+// let a = 18
+// let b = 18
+// let c = a <= b
+// console.log(c)
+
+// console.log(5%133);
+// BODMAS
+// console.log(2*2-2);
+
+// console.log(2+2*4+1);
+// -7
+// 2-8+1
+// -6+1
+// -5
+
+
+
+
+// comparison operators
+
+// ==
+// ===
+
+
+// console.log(5=="10");
+// console.log(5==="5");
+
+
+
