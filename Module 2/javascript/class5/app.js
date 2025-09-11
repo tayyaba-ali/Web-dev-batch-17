5344f828d92459095ec009634a0cfc3ea78c4e19
tayyaba-ali <ta6402792@gmail.com>
2025-09-11 11:50:00 +0500

    class5 js done

---
+++ Module 2/javascript/class5/app.js
@@ -0,0 +1,136 @@
+// let number1 = 3;
+// let number2 = 10;
+
+// //  10 % 3 
+// let number3 = number1%number2
+// console.log(number3);
+
+
+// INCREMENT / DECREMENT
+
+// let num = 3
+// // num = num+2
+// // num++
+
+// console.log(num-- + num--);
+
+// // 3 + 2
+// //3 +    4
+
+// console.log(num);
+
+// 5  8 
+
+
+// Tasks
+
+// let a = 1 
+// let b = 2 //3 
+// // 3 
+// let c = 3
+// let num = 2
+
+// // 3-2
+// //    3  - 3 + 1 + 2 + 3 - 2 - 2 + 4
+
+// console.log(++num - num + a +b++ +c - ++a - --b + ++c );
+
+
+// 3 - 3 + 1 + 2 +3 - 2 - 2 + 4 
+// 0 + 1 + 2 +3 - 2 - 2 + 4 
+// 3 +3 -2 -2 +4 
+// 6 -2 -2 +4 
+// 4 -2 +4 
+// 2 +4 
+// 6 
+
+
+
+
+
+
+
+
+// console.log(a-- - b++ + ++b + a);
+
+// 1 -   2 + 4 + 0
+
+//  -1 +4 +0
+//  3+0
+// 3
+
+// -5  3  2  5 -4  4  
+
+
+
+// let num  =  1;
+
+
+// document.write(`4 X ${num} = ${4*num}`)
+// num++ 
+// document.write("<br />")
+// document.write(`4 X ${num} =  ${4*num}`)
+// num++ 
+// document.write("<br />")
+// document.write(`4 X ${num} =  ${4*num}`)
+// num++ 
+// document.write("<br />")
+// document.write(`4 X ${num} =  ${4*num}`)
+// num++ 
+// document.write("<br />")
+// document.write(`4 X ${num} =  ${4*num}`)
+// num++ 
+// document.write("<br />")
+// document.write(`4 X ${num} =  ${4*num}`)
+// num++ 
+// document.write("<br />")
+// document.write(`4 X ${num} =  ${4*num}`)
+// num++ 
+// document.write("<br />")
+// document.write(`4 X ${num} =  ${4*num}`)
+// num++ 
+// document.write("<br />")
+// document.write(`4 X ${num} =  ${4*num}`)
+// num++ 
+// document.write("<br />")
+// document.write(`4 X ${num} =  ${4*num}`)
+
+
+
+// comparison operators
+
+// >
+// <
+// ==
+// ===
+// <=
+// >=
+// !=
+// !==
+
+// console.log(!true);
+// console.log(!false);
+
+// console.log(2>=3);
+
+// === opposite
+
+// console.log("cat" > "car");
+// 116>114
+
+// console.log(2 !== "10");
+
+
+
+// let num = "2"
+// console.log(typeof num);
+
+
+// prompt
+
+var number1 = +prompt("Whats your number");
+// console.log(typeof studentName);
+console.log(number1 + 2);
+ 
+
+
