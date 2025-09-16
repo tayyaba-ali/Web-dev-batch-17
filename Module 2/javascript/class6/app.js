42e239ed1328955028e57c5263d4f1c5bd42889d
tayyaba-ali <ta6402792@gmail.com>
2025-09-16 12:16:28 +0500

    class 6 done

---
+++ Module 2/javascript/class6/app.js
@@ -0,0 +1,162 @@
+// let number1 = prompt("give us your number")
+// let number = +number1+3
+// console.log(number);
+
+
+// let userAge = +prompt("apke age kya ha?") //20
+
+// if(userAge >= 18){
+//   alert("ap jhula jhool skte hain")
+// }
+// else{
+//     alert("you are not allowed")
+// }
+
+
+// if (false) {
+//     console.log("if chlrha ha");
+
+// }
+
+
+// let userCity = prompt("whats your city name")
+
+// // "lahore" === "karachi"
+// if (userCity === "karachi") {
+//     alert("kl se ajao")
+// }
+// else {
+//     alert("karachi ajao")
+// }   
+
+
+// let food= prompt("what you wanna eat")
+
+// if(food === "biryani"){
+//     alert("dil garden garden hogya")
+// }
+// else if(food == "andeWalaBurger"){
+//     alert("2 le ana")
+// }
+// else if(food == "pizza"){
+//     alert("1 le ana")
+
+// }
+// else if(food == "lays"){
+//     alert("3 le ana")
+// }
+
+// else{
+//     alert("french fries le ana")
+// }
+
+// let marks = 20;
+// if(marks >= 80){
+//     console.log("your Grade is A")
+
+// }else if(marks>= 60 ){
+//     console.log("Your Grade is B")
+// }else if (marks >= 40){
+//     console.log("Your Grade is C")
+// }else{
+//     console.log("need hard work")
+// }
+
+// let weather = prompt("Enter your city weather");
+// if(weather == "sunny"){
+//     document.write("you should wear your glasses")
+
+// }else if(weather == "Rainy"){
+//     document.write("take your umbrella")
+// }else if (weather == "cold"){
+//     document.write("you should wear your jacket")
+// }else{
+//     document.write("weather is unpredictable please live at home")
+// }
+// let month = prompt("enter your birthday month")
+// if (month === "september"){
+//     alert("happy birthday (larib)")
+// } else if(month === "october"){
+//     alert("happy birthday inadvance (sana)")
+
+// }else{
+//     alert("we wish you later")
+// }
+
+
+// let amount = 50000
+// let withdraw = + prompt("enter your amount" )
+// if(amount == withdraw ){
+//     alert("withdraw sucessful")
+// }else if(amount > withdraw){
+//     alert("withdraw done")
+// }else{
+//     alert("insufficent amount")
+// }
+
+
+// let number = 14
+
+// if (number !== 14) {
+//     console.log("hello");
+
+// }
+// else {
+//     console.log("hi");
+
+// }
+
+
+
+// if(54545){
+//     console.log("running");
+
+// }
+
+
+// falsy values
+// 0
+// ""
+// false 
+// null
+// undefined
+// NaN
+
+// console.log(Boolean(0538));
+
+
+// let userData = +prompt("whats your name")
+
+// // console.log(userData);
+
+// // if("")
+
+// // "0"
+// if (userData) {
+//     console.log("if chlrha ha");
+
+// }
+// else {
+//     console.log("write something");
+
+// }
+
+// logical operator
+// &&  and
+// ||  or
+
+
+
+console.log(0 && "tayyaba" || 7 && 34);
+
+// 0 Alveena 
+//false  true = true
+//true  wania
+// 0 || 34
+// 34 Anoosha
+// 0  true  34 7 
+// 0 true 7 ghazala
+// true || true
+// true
+
+
