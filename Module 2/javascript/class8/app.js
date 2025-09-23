9fcb1f02f044492e835f8130e8458607d0b67d74
tayyaba-ali <ta6402792@gmail.com>
2025-09-23 12:27:21 +0500

    class 8 done

---
+++ Module 2/javascript/class8/app.js
@@ -0,0 +1,89 @@
+// const age = 20
+// const result = ( age < 13 ) ? " Adult" : ( age <= 18 ) ? "Minor"  :
+// "somthing"
+// alert ( result )
+// let marks = 95;
+
+// let grade = (marks >= 90) ? "A+"
+// : (marks >= 80) ? "A"
+// : (marks >= 70) ? "B"
+// : (marks >= 60) ? "C"
+// : "Fail"
+
+// console.log(grade);
+
+// Ternary operator
+
+// let age = 8;
+// let drink  = (age>10)?"coffee": (age==10)?"juice" :"milk";
+// console.log(drink);
+
+// if(age>10){
+//  drink = "coffee"
+// }
+// else if (age == 10 ){
+//     drink = "juice"
+// }
+// else{
+//     drink = "milk"
+// }
+// console.log(drink);
+
+// const colors = ["red","pink","purple"]
+// //  index        0       1     2
+// // length        1       2     3
+// // console.log(colors.length);
+// // colors[5]="blue";
+// // console.log(colors);
+
+// colors[2] = "black"
+// console.log(colors);
+
+// let arr = [1,2,3,4,5,5,6]
+// arr.unshift( 7,8,9)
+// console.log(arr)
+//               0          1        2      3       4        5
+// let fruits  = ["banana", "apple" ,"kiwi","mango","orange","peech"]
+// let newFruits = fruits.slice(4)
+// console.log(fruits)
+// console.log(newFruits)
+
+// let rollNumber = [1,2,3,4,5,6,7,8]
+//  let NewRollNumbers = rollNumber.slice(5)
+//  console.log(NewRollNumbers);
+
+// let students = ["sadia", "fatima", "erum", "asia","sadaf"]
+// students.splice(2)
+// console.log(students);
+
+// let myName = "Tayyaba";
+// //              -4 -3  -2  -1
+
+// console.log(myName.slice(-2));
+
+
+
+// myName.pop();
+// console.log(myName);
+
+const numbers = [1,2,389,"Tayyaba"]
+//               0  1   2
+//               1  2   3
+
+console.log(numbers[numbers.length-1]);
+
+// 0     1      2
+// 1     2      3
+
+
+// numbers.splice(2,0,10)
+// console.log(numbers);
+
+// console.log(numbers.pop());
+// console.log(numbers.push("Umra","Minahil"));
+
+// console.log(numbers.unshift(0,67));
+// console.log(numbers.shift());
+
+
+// console.log(numbers);
