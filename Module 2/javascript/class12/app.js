a4d38b467bdc5140d61a0def4d77a2a71c8d51bd
tayyaba-ali <ta6402792@gmail.com>
2025-10-07 12:04:03 +0500

    class12 done

---
+++ Module 2/javascript/class12/app.js
@@ -0,0 +1,122 @@
+// let city = "karachi"
+// console.log(city === "kArachi");
+
+// let city  = prompt("Whats your city") //karachi
+
+// // KARACHI (uppercase)
+// // karachi (lowercase)
+// // Karachi (capatilize)
+// if(city === "KARACHI"){
+//     console.log("Welcome to the city of lights");
+
+// }
+
+// let city = "lahore"
+
+// console.log(city.toLocaleLowerCase());
+// let studentName = "sadia khan";
+// newName=studentName.toUpperCase();
+// console.log(newName);
+
+// let city = prompt("Enter any city name");
+// city=city.toLowerCase();
+
+// if(city==="karachi"){
+//     console.log(`${city} is the biggest city of pakistan`)
+
+// }else{
+//     console.log(`${city} is other city of pakistan`)
+// }
+
+// aSiA
+// let studentName =prompt("Enter mostly absent student name").toUpperCase();
+// // studentName = studentName.toUpperCase();
+// if(studentName.toUpperCase() === "asia"){
+//     console.log(`${studentName} is motsly absent student`)
+// }else{
+//     console.log(`${studentName} is puncatul student`)
+// }
+
+// let userKhuahish = prompt("What you wanna eat").toLowerCase()
+
+// const bakeryItems = ["cake","pasteries","chips","cookies","slice"]
+// let flag = false
+
+// for(let i = 0 ; i<bakeryItems.length;i++){
+
+//     if(userKhuahish == bakeryItems[i]){
+//         flag = true
+//     }
+// }
+// if(flag){
+//     console.log("yes available");
+
+// }
+// else{
+//     console.log("nh ha");
+
+// }
+
+// karachi
+// Karachi
+
+// keyboard
+// Keyboard
+
+let userName = "tAyYaBa";
+//             T                             +  ayyaba
+let final =
+  userName.slice(0, 1).toUpperCase() + userName.slice(1).toLowerCase();
+// userName.slice(0,1)
+// userName[0]
+
+// let firstLetter = userName[0].toUpperCase()
+// let restLetters = userName.slice(1).toLowerCase()
+// let final = firstLetter + restLetters
+// // console.log(firstLetter);
+// // console.log(restLetters);
+
+console.log(final);
+
+// const months = [
+//   "January",
+//   "February",
+//   "March",
+//   "April",
+//   "May",
+//   "June",
+//   "July",
+//   "August",
+//   "September",
+//   "October",
+//   "November",
+//   "December",
+// ];
+
+// for(let i = 0 ; i< months.length;i++){
+//     if(months[i].length===3){
+//         document.write(months[i].slice(0,3).toUpperCase()+"<br />")
+//     }
+// }
+
+
+// split (toorna)
+// string ko toorega or array me convert krdega
+
+// let word = "Hello World we are humans"
+
+// // []
+
+// console.log(word.split(" "));
+
+
+// join
+
+// const colors = ["pink","purple","white","green"]
+// // pink purple white green 
+// console.log(colors.join(" "));
+
+
+// Task 
+
+// aik paragraph ho use capatilize me convert krna ha 
